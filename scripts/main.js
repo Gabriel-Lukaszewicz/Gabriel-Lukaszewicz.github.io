78f0ffd6355aaf558aeb16d2ede000886a098a7e
Gabriel-Lukaszewicz <60377359+Gabriel-Lukaszewicz@users.noreply.github.com>
2020-07-01 00:42:44 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+
+// alert("what's good bitch"); //makes a pop-up box appear in the browser when loading the page
+
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/joe-biden.jpg') {
+        myImage.setAttribute('src', 'images/donald-trump.png');
+    } else {
+        myImage.setAttribute('src', 'images/joe-biden.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Grundle Huncho approves of ' + myName + ' ya little bitch.';
+    }
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
